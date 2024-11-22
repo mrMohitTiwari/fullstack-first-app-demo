77b545dbe3e78e3dbf695ee5010e31c89f848a97
mrMohitTiwari <mt0988145@gmail.com>
2024-11-23 00:25:44 +0530

    maintaing the state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
   // state for getting and setting jokes
   const [jokes, setJokes] = useState([]);
   // getting the data from the backend using axioms
+  useEffect(() => {
+    axios.get("http://localhost:3000/joke").then((res) => {
+      setJokes(res.data);
+      console.log(res.data);
+    });
+  });
 
   return (
     <>
